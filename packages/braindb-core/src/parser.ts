dfd137a9df54f9d7734c6cd6ceac07a928e30c70
stereobooster <stereobooster@gmail.com>
2024-02-07 19:35:10 +0100

    use fork of remark-wiki-link

--- packages/braindb-core/src/parser.ts
+++ packages/braindb-core/src/parser.ts
@@ -1,8 +1,7 @@
 import { unified } from "unified";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkParse from "remark-parse";
-// @ts-expect-error
-import wikiLinkPlugin from "remark-wiki-link";
+import wikiLinkPlugin from "@stereobooster/remark-wiki-link";
 import remarkStringify from "remark-stringify";
 // import remarkMdx from "remark-mdx"
 
@@ -11,12 +10,6 @@ export const mdParser = unified()
   .use(remarkParse)
   // @ts-expect-error
   .use(remarkFrontmatter)
-  .use(wikiLinkPlugin, {
-    hrefTemplate: (permalink: any) => permalink,
-    pageResolver: (name: any) => [decodeURI(name).toLowerCase()],
-    aliasDivider: "|",
-    wikiLinkClassName: " ",
-    newClassName: " ",
-  })
+  .use(wikiLinkPlugin, { aliasDivider: "|" })
   // @ts-expect-error
   .use(remarkStringify, { resourceLink: false });
