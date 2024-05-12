78d0ad56072341a3c74d98d36aa2707000195d05
stereobooster <stereobooster@gmail.com>
2024-05-12 12:10:51 +0200

    mermaid diagrams

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import { rehypeMermaid } from "@beoe/rehype-mermaid";
+import { getCache } from "@beoe/cache";
+
+const cache = await getCache();
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://braindb.stereobooster.com/",
@@ -12,6 +17,7 @@ export default defineConfig({
         light: "./src/assets/logo.svg",
         dark: "./src/assets/logo-dark.svg",
       },
+      customCss: ["./src/styles/custom.css"],
       title: "BrainDB",
       social: {
         github: "https://github.com/stereobooster/braindb",
@@ -30,6 +36,14 @@ export default defineConfig({
       ],
     }),
   ],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeMermaid,
+        { class: "not-content", strategy: "img-class-dark-mode", cache },
+      ],
+    ],
+  },
   vite: {
     optimizeDeps: {
       exclude: ["fsevents", "@node-rs", "@napi-rs"],
